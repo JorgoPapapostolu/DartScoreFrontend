5eb19f0401de94832412980fa4ea195596e72872
JorgoPapapostolu <jorgopapapostolu@gmail.com>
2023-04-19 19:02:25 +0200

    Registration

--- src/components/signuppage/signup.tsx
+++ src/components/signuppage/signup.tsx
@@ -1,27 +1,69 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
 import "./signup.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+interface SignupFormData {
+  vorname: string;
+  nachname: string;
+  email: string;
+  password: string;
+}
 
 export default function Signup() {
+  const [formData, setFormData] = useState<SignupFormData>({
+    vorname: "",
+    nachname: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    axios
+      .post("https://fluffy-fly-bedclothes.cyclic.app/user/signup", formData)
+      .then((response) => {
+        console.log(response.data);
+        navigate('/login');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleInputChange = (event: FormEvent<HTMLInputElement>) => {
+    const { id, value } = event.currentTarget;
+    setFormData((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+  };
 
   return (
     <div className="signup-container">
-      <form className="signup-form">
+      <form className="signup-form" onSubmit={handleSubmit}>
         <h2>Registriere dich noch heute!</h2>
         <div className="form-group-signup">
-          <label htmlFor="firstname">Vorname</label>
+          <label htmlFor="vorname">Vorname</label>
           <input
             type="text"
-            id="firstname"
+            id="vorname"
             required
+            value={formData.vorname}
+            onChange={handleInputChange}
           />
         </div>
         <div className="form-group-signup">
-          <label htmlFor="lastname">Nachname</label>
+          <label htmlFor="nachname">Nachname</label>
           <input
             type="text"
-            id="lastname"
+            id="nachname"
             required
+            value={formData.nachname}
+            onChange={handleInputChange}
           />
         </div>
         <div className="form-group-signup">
@@ -30,6 +72,8 @@ export default function Signup() {
             type="email"
             id="email"
             required
+            value={formData.email}
+            onChange={handleInputChange}
           />
         </div>
         <div className="form-group-signup">
@@ -38,6 +82,8 @@ export default function Signup() {
             type="password"
             id="password"
             required
+            value={formData.password}
+            onChange={handleInputChange}
           />
         </div>
         <button type="submit" className="btn">
@@ -49,5 +95,4 @@ export default function Signup() {
       </form>
     </div>
   );
-};
-
+}
